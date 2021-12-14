6de3c3cd63e0cc364fcd4117b458634b0fe38c7e
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2021-12-13 22:26:00 -0300

    Widget Resposta with function

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() onSelect;
 
-  const Resposta(this.texto, {Key? key}) : super(key: key);
+  const Resposta(this.texto, this.onSelect, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const ElevatedButton(
-      child: Text('Resposta 1'),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        child: const Text('Resposta 1'),
+        onPressed: onSelect,
+        style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            textStyle: const TextStyle(color: Colors.white)),
+      ),
     );
   }
 }
