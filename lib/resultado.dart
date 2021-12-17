b4d8ea1de4b88f3a60753dba2d1d767d7d0e4d6b
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2021-12-16 21:59:42 -0300

    Restart app

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
   final int pontuacaoTotal;
-  const Resultado(this.pontuacaoTotal, {Key? key}) : super(key: key);
+  final void Function() quandoReiniciarFormulario;
+  const Resultado(this.pontuacaoTotal, this.quandoReiniciarFormulario,
+      {Key? key})
+      : super(key: key);
 
   String get fraseResultado {
     if (pontuacaoTotal < 8) {
@@ -18,11 +21,23 @@ class Resultado extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        fraseResultado,
-        style: const TextStyle(fontSize: 28),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: const TextStyle(fontSize: 28),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: quandoReiniciarFormulario,
+          child: const Text(
+            "Reiniciar?",
+            style: TextStyle(fontSize: 18),
+          ),
+        )
+      ],
     );
   }
 }
